9d5609b9b1a26017bc1bc131fa2902db520090bf
Lingyan Jiang <lingyanj@bu.edu>
2020-09-28 16:50:32 -0400

    exercise - add read.py and sim.py

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -2,5 +2,13 @@ def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
-    """
-    raise NotImplementedError()
+    ""
+matrix = []
+with open(csv_file_path, "r") as m:
+    lines = m.readlines()
+    for line in lines:
+        line = line.split(",")
+        for i in range (len(line)):
+            line[i]=eval(line[i])
+            matrix.append(line)
+return matrix
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,13 +1,43 @@
 def euclidean_dist(x, y):
-    raise NotImplementedError()
+    m = 0
+    for i ,j in zip(x,y):
+        m += (i-j) ** 2
+    return m ** 0.5
+
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    m =0
+    for i ,j in zip(x,y):
+        m += abs(i-j)
+    return m ** 1
+
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    n = 0
+    u = 0
+    if len(x) != len(y)
+    return -1
+    if len(x) == 0
+    return -1
+    for i in range(len(x)):
+        if x[i] == y[i]:
+            n += 1;
+        u += 1;
+    return 1-n/u
+
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    if len(x) != len(y) 
+    return -1
+    if len(x) == 0
+    return -1
+    mul = 0;
+    norm1 = 0;
+    norm2 = 0;
+    for i in range(len(x)):
+        mul += x[i] * y[i];
+        norm1 += x[i] ** 2;
+        norm2 += y[i] ** 2;
+    return mul/((norm1 ** 0.5) * (norm2 ** 0.5))
 
 # Feel free to add more
